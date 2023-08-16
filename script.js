83e71d029c655d9a49986f59b9241de02823b0d5
mtalli <mauno.talli@hotmail.com>
2023-08-16 07:44:22 +0300

    Game done, CSS to be added later on

--- script.js
+++ script.js
@@ -12,11 +12,20 @@ const Gameboard = (() => {
     };
   
     const checkWin = (marker) => {
-     
+      const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]            // Diagonals
+      ];
+  
+      return winningCombinations.some(combination => {
+        const [a, b, c] = combination;
+        return board[a] === marker && board[b] === marker && board[c] === marker;
+      });
     };
   
     const checkTie = () => {
-      
+      return board.every(cell => cell !== "");
     };
   
     return {
@@ -30,30 +39,57 @@ const Gameboard = (() => {
   const Player = (name, marker) => {
     return { name, marker };
   };
-  
-  const DisplayController = (() => {
+
+const DisplayController = (() => {
     const cells = document.querySelectorAll(".cell");
+    const startButton = document.getElementById("startButton");
+    const player1NameInput = document.getElementById("player1Name");
+    const player2NameInput = document.getElementById("player2Name");
+    const messageDisplay = document.getElementById("message");
   
-    cells.forEach((cell, index) => {
-      cell.addEventListener("click", () => {
-        if (!Gameboard.updateCell(index, currentPlayer.marker)) {
-          return;
-        }
+    let player1, player2, currentPlayer;
+    let gameEnded = false; 
   
-        cell.textContent = currentPlayer.marker;
-        if (Gameboard.checkWin(currentPlayer.marker)) {
-          
-        } else if (Gameboard.checkTie()) {
-          
-        } else {
-          
-          currentPlayer = currentPlayer === player1 ? player2 : player1;
-        }
+    startButton.addEventListener("click", () => {
+      player1 = Player(player1NameInput.value, "X");
+      player2 = Player(player2NameInput.value, "O");
+      currentPlayer = player1;
+      messageDisplay.textContent = `${currentPlayer.name}'s turn`;
+  
+      cells.forEach((cell, index) => {
+        cell.textContent = "";
+        cell.addEventListener("click", () => handleCellClick(cell, index));
       });
-    });
-  })();
   
-  let player1 = Player("Player 1", "X");
-  let player2 = Player("Player 2", "O");
-  let currentPlayer = player1;
+      startButton.disabled = true;
+      gameEnded = false; 
+    });
+  
+    const handleCellClick = (cell, index) => {
+      if (gameEnded || !Gameboard.updateCell(index, currentPlayer.marker)) {
+        return;
+      }
+  
+      cell.textContent = currentPlayer.marker;
+      if (Gameboard.checkWin(currentPlayer.marker)) {
+        messageDisplay.textContent = `${currentPlayer.name} wins!`;
+        disableCellClick();
+        gameEnded = true; 
+      } else if (Gameboard.checkTie()) {
+        messageDisplay.textContent = "It's a tie!";
+        disableCellClick();
+        gameEnded = true; 
+      } else {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        messageDisplay.textContent = `${currentPlayer.name}'s turn`;
+      }
+    };
+  
+    const disableCellClick = () => {
+      cells.forEach((cell) => {
+        cell.removeEventListener("click", handleCellClick);
+      });
+      startButton.disabled = false;
+    };
+  })();
   
